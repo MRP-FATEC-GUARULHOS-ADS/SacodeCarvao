7d768eac3fc38bc0cf5fbc8709f154b3130ba2a6
MARCOS SILVA <marcos.silva312@fatec.sp.gov.br>
2022-09-15 13:33:31 -0300

    criado o 1 controller

---
+++ MRP_SdC/Controladores/ComponenteControlador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MRP_SdC.Telas;
+
+namespace MRP_SdC.Controladores
+{
+    class ComponenteControlador
+    {
+        public void cadastrarComponente(Componente componente)
+        {
+            // confirmando insercao
+            DialogResult confirmarInsert = MessageBox.Show(
+                componente.modelo + " ?!", "Confirmar Inserção",
+                MessageBoxButtons.YesNo
+            );
+            if (confirmarInsert == DialogResult.Yes)
+            {
+                MySQL.ComponenteDAO objCompDAO = new MySQL.ComponenteDAO();
+                objCompDAO.Insert(componente);
+            }
+        }
+
+        public void verificarCamposVazios(string tipo, string marca, string modelo, Button ok)
+        {
+            if (tipo != "" && marca != "" && modelo != "")
+            {
+                ok.Enabled = true;
+            }
+            else
+            {
+                ok.Enabled = false;
+            }
+        }
+
+        public void checarEstado(ItemCheckEventArgs e, CheckedListBox estado, int ix)
+        {
+            for (ix = 0; ix < estado.Items.Count; ++ix)
+                if (ix != e.Index) estado.SetItemChecked(ix, false);
+        }
+    }
+}
--- MRP_SdC/Telas/Componentes/CadastroComponente.cs
+++ MRP_SdC/Telas/Componentes/CadastroComponente.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MRP_SdC.Controladores;
 
 namespace MRP_SdC
 {
     public partial class CadastroComponente : Form
     {
+        ComponenteControlador compControl = new ComponenteControlador();
+
+        Componente myComponente = new Componente();
+
         public CadastroComponente()
         {
             InitializeComponent();
@@ -23,40 +28,24 @@ namespace MRP_SdC
         // funcoes das text-boxes
         private void Required_TextChanged(object sender, System.EventArgs e)
         {
-            if (tipo_cbb.Text != "" && marca_tbx.Text != "" && modelo_tbx.Text != "")
-            {
-                ok_btn.Enabled = true;
-            }
-            else
-            {
-                ok_btn.Enabled = false;
-            }
+            compControl.verificarCamposVazios(tipo_cbb.Text, marca_tbx.Text, modelo_tbx.Text, ok_btn);
         }
 
         // funcoes da checklist
         private void Estado_ItemCheck(object sender, ItemCheckEventArgs e)
         {
-            for (int ix = 0; ix < estado_clb.Items.Count; ++ix)
-                if (ix != e.Index) estado_clb.SetItemChecked(ix, false);
+            int ix=0;
+            compControl.checarEstado(e, estado_clb, ix);
         }
 
         // funcoes dos botoes
         private void OK_btn_Click(object sender, System.EventArgs e)
         {
-            Componente myComponente = new Componente(
+            myComponente = new Componente(
                 tipo_cbb.Text, marca_tbx.Text, modelo_tbx.Text, especs_tbm.Text, (estado_clb.CheckedIndices[0] == 0 ? true : false)
             );
 
-            // confirmando insercao
-            DialogResult confirmarInsert = MessageBox.Show(
-                "〜(￣▽￣〜) " + myComponente + " ?!", "Confirmar Inserção",
-                MessageBoxButtons.YesNo
-            );
-            if (confirmarInsert == DialogResult.Yes)
-            {
-                MySQL.ComponenteDAO objCompDAO = new MySQL.ComponenteDAO();
-                objCompDAO.Insert(myComponente);
-            }
+            compControl.cadastrarComponente(myComponente);
 
             Close();
         }
